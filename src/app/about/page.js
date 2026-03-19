1db55c6b9d2ccd99c425c4143b6adaa9f3d4da7b
DesignerPuspita <157014828+DesignerPuspita@users.noreply.github.com>
2026-03-19 17:24:31 +0530

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,45 @@
-import React from 'react'
+// app/contact/page.js
+import AboutUs from '@/components/About'
+import { getDataService, postService } from '@/services/service'
 
-const about = () => {
+
+export async function generateMetadata() {
+  const seoRes = await postService('get-seo-by-slug', 'about')
+
+  const seo = await seoRes?.data?.seometa
+  return {
+    title: seo?.title || "About Us",
+    description:
+      seo?.description ||
+      "BEAS Consultancy is a global IT and consulting firm providing information solutions to businesses across industries.",
+  
+    openGraph: {
+      title: seo?.title,
+      description: seo?.description,
+      images: seo?.image
+        ? [`${env.BACKEND_BASE_URL}${seo.image}`]
+        : [],
+  
+      keywords: seo?.keyword
+        ? seometadata.keyword
+        : "About Beas, Meet our team, Software Company, Technology, Experts at Beas, Corporate Profile",
+  
+      authors: seo?.author
+        ? [seo.author]
+        : ["BEAS Consultancy And Services Private Limited"]
+    }
+  };
+}
+
+const Page = async () => {
+
+  const [aboutData, commonAboutData] = await Promise.all([getDataService('get-menu-aboutus'), getDataService('get-common-aboutus')]);
   return (
     <div>
-      about
+
+      <AboutUs aboutus={aboutData?.data?.aboutus} commonaboutus={commonAboutData?.data?.commonaboutus} />
     </div>
   )
 }
 
-export default about
+export default Page
---
+++ src/components/About/index.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Image from 'next/image';
+import BreadCrumb from '../BreadCrumb';
+// import BreadCrumb from '../component/BreadCrumb';
+// import SEO from '../../components/SEO';
+
+// import HomeService from '../../util/service/Home';
+import { env } from '../../util/constants/common';
+
+const AboutUs = ({ aboutus, commonaboutus, seometadata }) => {
+
+    // ---- Split description once (server-side safe)
+    const descriptionText = aboutus?.description
+        ? aboutus.description.split(' ').slice(0, 139).join(' ')
+        : '';
+
+    const descriptionTextTwo = aboutus?.description
+        ? aboutus.description.split(' ').slice(139).join(' ')
+        : '';
+
+
+    const metaTitle = seometadata?.title
+        ? seometadata?.title
+        : `About Us`;
+    const metaKeyword = seometadata?.keyword
+        ? seometadata?.keyword
+        : "About Beas, Meet our team, Software Company, Technology, Experts at Beas, Corporate Profile";
+    const metaDesc = seometadata?.description
+        ? seometadata?.description
+        : "Learn more about Beas and our specialties.";
+    const metaImage = seometadata?.image
+        ? `${env.BACKEND_BASE_URL}${seometadata?.image}`
+        : `${env.BACKEND_BASE_URL}${aboutus?.image}`;
+    const metaUrl = seometadata?.url
+        ? `${env.FRONTEND_BASE_URL}${seometadata?.url}`
+        : `${env.FRONTEND_BASE_URL}about`;
+    const metaAuthor = seometadata?.author
+        ? seometadata?.author
+        : "BEAS Consultancy And Services Private Limited";
+
+    return (
+        <>
+            {/* <SEO
+                title={metaTitle}
+                description={metaDesc}
+                keywords={metaKeyword}
+                image={metaImage}
+                url={metaUrl}
+                author={metaAuthor}
+            /> */}
+
+            <main>
+                {/* <BreadCrumb
+                    pagetitle="About Us"
+                    pageBanner={`assets/img/menu-content/${aboutus?.menu_contents?.banner || ''}`}
+                /> */}
+                <BreadCrumb pagetitle="About Us" pageBanner={aboutus?.menu_contents?.banner} />
+                {/* ===== About Intro ===== */}
+                <section className="section-padding">
+                    <Container>
+                        <Row>
+                            <Col lg={5} md={5}>
+                                <div className="about_imgs">
+                                    <span>
+                                        <Image
+                                            src={`${env.BACKEND_BASE_URL}${aboutus?.image}`}
+                                            alt="About Beas"
+                                            width={500}
+                                            height={200}
+                                            priority
+                                            className="img-fluid about_thumb_img"
+                                        />
+                                    </span>
+                                </div>
+                            </Col>
+
+                            <Col lg={7} md={7}>
+                                <div className="about_texts">
+                                    <h1>{aboutus?.menu_contents?.title}</h1>
+                                    <div
+                                        dangerouslySetInnerHTML={{ __html: descriptionText }}
+                                    />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+
+                {/* ===== Description Continued ===== */}
+                <section className="section-abuts">
+                    <Container>
+                        <Row>
+                            <Col>
+                                <div className='ser_rea'>
+                                    <div
+                                        className="about_texts"
+                                        suppressHydrationWarning
+                                        dangerouslySetInnerHTML={{ __html: descriptionTextTwo }}
+                                    />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+
+                    <div className="shp1">
+                        <Image
+                            src="/assets/images/ser-bg.png"
+                            alt="shape"
+                            width={474}
+                            height={73}
+                            loading="lazy"
+                        />
+                    </div>
+
+                    <div className="shp2">
+                        <Image
+                            src="/assets/images/ser-bg2.png"
+                            alt="shape"
+                            width={474}
+                            height={73}
+                            loading="lazy"
+                        />
+                    </div>
+                </section>
+
+                {/* ===== Mission / Strategy ===== */}
+                <div className="why_abt">
+                    <Container>
+                        <Row className="align-items-center">
+                            <Col md={6}>
+                                <div className="our_misson_boxs">
+                                    {aboutus?.menu_contents?.contents
+                                        ?.filter(item => item.extra_title !== 'Corporate Strategy')
+                                        ?.map((item, index) => (
+                                            <div key={index} className="mission_boxs our_box_mi">
+                                                <h2>{item.extra_title}</h2>
+                                                <p>{item.extra_description}</p>
+                                            </div>
+                                        ))}
+                                </div>
+                            </Col>
+
+                            <Col md={6}>
+                                {aboutus?.menu_contents?.contents
+                                    ?.filter(item => item.extra_title === 'Corporate Strategy')
+                                    ?.map((item, index) => (
+                                        <div key={index} className="why_abuts">
+                                            <h2>{item.extra_title}</h2>
+                                            <div
+                                                dangerouslySetInnerHTML={{
+                                                    __html: item.extra_description
+                                                }}
+                                            />
+                                        </div>
+                                    ))}
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+
+                {/* ===== Counters ===== */}
+                <section className="meter-area">
+                    <Container>
+                        <h2>
+                            We've helped businesses increase their revenues significantly
+                            in their very first year of partnering with us!
+                        </h2>
+
+                        <div className="counter-show">
+                            <Row>
+                                <div className='all-meter'>
+                                    <ul>
+                                        {commonaboutus
+                                            ?.filter(item =>
+                                                ['Clients', 'Experts', 'Projects', 'Years Experience'].includes(item.title)
+                                            )
+                                            ?.map((item, index) => (
+                                                <li key={index} className="meter-box">
+                                                    <span className="timer count-number">
+                                                        {item.short_desc}
+                                                    </span>
+                                                    <p>{item.long_desc}</p>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                </div>
+                            </Row>
+                        </div>
+                    </Container>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default AboutUs;
+// export async function getStaticProps() {
+//     try {
+//         const [homeres, aboutRes, commonRes, seoRes] = await Promise.all([
+//             HomeService.homePage(),
+//             HomeService.menuAboutusPage(),
+//             HomeService.commonaboutusPage(),
+//             HomeService.seobyslug('about')
+//         ]);
+
+//         return {
+//             props: {
+//                 homeData: homeres?.data || null,
+//                 aboutus: aboutRes?.data?.aboutus || null,
+//                 commonaboutus: commonRes?.data?.commonaboutus || [],
+//                 seometadata: seoRes?.data?.seometa || null
+//             },
+//             revalidate: 600 // 10 minutes
+//         };
+//     } catch (error) {
+//         return { notFound: true };
+//     }
+// }

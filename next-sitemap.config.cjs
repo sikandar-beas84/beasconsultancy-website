98abecb165dd5d5418558e261051cabe3db5310a
Sikandar Beas <sikandar.beas@gmail.com>
2026-03-24 10:53:43 +0530

    sitemap

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,9 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://beta.beasconsultancy.com',
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
